f9e24e8388708aad07758836c64ce7e58618b218
Jose Medina <josesaygg@gmail.com>
2024-08-04 19:09:42 -0500

    calculator v1

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello world FROM calculator3!");
-});
-
-const calculateCostAndSavings = () => {
-  console.log("Hello world!");
-};
